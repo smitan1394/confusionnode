dee3532fd36d689f9e378f7d0001361c5b477db8
Smita Nargundkar <nsharatc@in.ibm.com>
2019-10-15 16:37:45 +0530

    assignment 3

--- routes/users.js
+++ routes/users.js
@@ -61,8 +61,18 @@ router.get('/logout', (req, res) => {
 });
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({})
+.then((users) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(users);
+})
+.catch((err) => {
+  res.statusCode = 500;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({err: err});
+});
 });
 
 module.exports = router;
