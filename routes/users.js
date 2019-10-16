b186f778f72976caa533491853c656a57fbd2da0
Smita Nargundkar <nsharatc@in.ibm.com>
2019-10-16 16:32:14 +0530

    cors

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const bodyParser = require('body-parser');
+const cors = require('./cors');
 var User = require('../models/user');
 var express = require('express');
 var passport = require('passport');
@@ -7,7 +8,22 @@ var router = express.Router();
 
 router.use(bodyParser.json());
 
-router.post('/signup', (req, res, next) => {
+/* GET users listing. */
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+  User.find({})
+.then((users) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(users);
+})
+.catch((err) => {
+  res.statusCode = 500;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({err: err});
+});
+});
+
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}),
     req.body.password, (err, user) => {
     if(err) {
@@ -38,7 +54,7 @@ router.post('/signup', (req, res, next) => {
 });
 
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
@@ -47,7 +63,7 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
 });
 
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
@@ -60,19 +76,5 @@ router.get('/logout', (req, res) => {
   }
 });
 
-/* GET users listing. */
-router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
-  User.find({})
-.then((users) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(users);
-})
-.catch((err) => {
-  res.statusCode = 500;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({err: err});
-});
-});
 
 module.exports = router;
