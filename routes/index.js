4af1d90117b3b05f3e8069e4002db0732454c4d7
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-11-13 15:14:47 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const path = require('path');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    res.sendFile(path.join(`../dist/conFusion/index.html`)); 
+    res.sendFile(`../dist/conFusion/index.html`); 
 });
 
 module.exports = router;
