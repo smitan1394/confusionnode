e930d135ac875616f9354aa08cc5ee7a3d4b9606
smitan1394 <42381355+smitan1394@users.noreply.github.com>
2019-11-14 12:07:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //app.use(cookieParser('12345-67890-09876-54321'));
-
+ app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
 app.use(passport.initialize());
 
 app.use('/', indexRouter);
